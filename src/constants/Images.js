b60461e5d1a7828756f56c3a0d77e3a19083094d
fmoraless <franticzero@gmail.com>
2024-04-18 21:45:10 -0400

    category images

---
+++ src/constants/Images.js
@@ -0,0 +1,8 @@
+export default {
+  FRIED_CHICKEN: require('../../assets/images/food-categories/fried_chicken.png'),
+  BURGER: require('../../assets/images/food-categories/burger.png'),
+  PIZZA: require('../../assets/images/food-categories/pizza.png'),
+  DESSERT: require('../../assets/images/food-categories/dessert.png'),
+  NOODLES: require('../../assets/images/food-categories/noodles.png'),
+  DRINKS: require('../../assets/images/food-categories/drinks.png'),
+};
