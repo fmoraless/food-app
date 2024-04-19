57d83934111d8276c54e0dba2924e2756ccf234b
fmoraless <franticzero@gmail.com>
2024-04-18 21:46:06 -0400

    add categories mocked data

---
+++ src/constants/Mock.js
@@ -0,0 +1,10 @@
+const CATEGORIES = [
+  { name: 'Pollo', logo: 'FRIED_CHICKEN' },
+  { name: 'Sandwichs', logo: 'BURGER' },
+  { name: 'Pizza', logo: 'PIZZA' },
+  { name: 'Postres', logo: 'DESSERT' },
+  { name: 'Bebidas', logo: 'DRINKS' },
+  { name: 'Fideos', logo: 'NOODLES' },
+];
+
+export default { CATEGORIES };
