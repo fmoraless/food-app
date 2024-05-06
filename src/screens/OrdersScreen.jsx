d6f962074b654f958e0110b11decf75ef61649cc
fmoraless <franticzero@gmail.com>
2024-05-06 01:22:50 -0400

    add screen to navigate

---
+++ src/screens/OrdersScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export const OrdersScreen = () => {
+  return (
+    <View>
+      <Text>Orders Component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
