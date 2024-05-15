5fbdcc8177b942a889c512c1b6dcf0ea1524c2c8
fmoraless <franticzero@gmail.com>
2024-05-15 16:24:57 -0400

    create api middleware on store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import shopReducer from '../features/Shop/shopSlice';
+import { shopApi } from '../services/shopService';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     shop: shopReducer,
+    [shopApi.reducerPath]: shopApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shopApi.middleware),
 });
+
+setupListeners(store.dispatch);
+
+export default store;
