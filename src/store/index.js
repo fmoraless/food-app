62cd41cb0c4d9c5af93abf0d3edd35136224f8d0
fmoraless <franticzero@gmail.com>
2024-05-19 21:14:47 -0400

    create cart slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import shopReducer from '../features/Shop/shopSlice';
+import cartReducer from '../features/Cart/cartSlice';
 import { shopApi } from '../services/shopService';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 const store = configureStore({
   reducer: {
     shop: shopReducer,
+    cart: cartReducer,
     [shopApi.reducerPath]: shopApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
