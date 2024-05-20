d80950899dffd9fd0e421679a90aeb9c9fe5d04e
fmoraless <franticzero@gmail.com>
2024-05-19 21:16:39 -0400

    count item badge info on bottom navigator icon

--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -1,5 +1,5 @@
-import { View, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ProductScreen } from '../screens/ProductScreen';
 import { HomeScreen } from '../screens/HomeScreen';
@@ -11,10 +11,35 @@ import { FontAwesome5, FontAwesome6 } from '@expo/vector-icons';
 import { OrdersScreen } from '../screens/OrdersScreen';
 import { CartStack } from './CartStackNavigator';
 import { OrderStack } from './OrderStackNavigator';
+import { useSelector } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
+  //const [itemsCount, setItemsCount] = useState(0);
+  const totalItemsQty = useSelector(
+    (state) => state.cart.value.totalItemsQuantity,
+  );
+
+  console.log({ cantidadTotalItemsCarrito: totalItemsQty });
+
+  /* const selectCartItemsCount = createSelector(
+    (state) => state.cart.items,
+    (items) => items.reduce((total, item) => total + item.quantity, 0),
+  ); */
+
+  //console.log('ITEMS-total', items);
+
+  /* if (items.length) {
+    const itemsCount = items.reduce((total, item) => total + item.quantity, 0);
+    console.log(itemsCount);
+    setItemsCount(itemsCount);
+  } */
+
+  /* const cartItemCount = useSelector(selectCartItemsCount);
+  console.log(cartItemCount); */
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
@@ -53,6 +78,11 @@ export const BottomTabNavigator = () => {
                   size={24}
                   color={focused ? Colors.DEFAULT_RED : Colors.DEFAULT_GREY}
                 />
+                {totalItemsQty !== null && totalItemsQty > 0 && (
+                  <View style={styles.badge}>
+                    <Text style={styles.badgeText}>{totalItemsQty}</Text>
+                  </View>
+                )}
               </View>
             );
           },
@@ -90,5 +120,21 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 25,
     borderTopLeftRadius: 25,
     height: 60,
+  },
+  badge: {
+    position: 'absolute',
+    right: -6,
+    top: -3,
+    backgroundColor: 'red',
+    borderRadius: 6,
+    width: 15,
+    height: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  badgeText: {
+    color: 'white',
+    fontSize: 10,
+    fontWeight: 'bold',
   },
 });
