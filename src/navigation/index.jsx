806b7793476689df4081ce790ecc93637dac3d18
fmoraless <franticzero@gmail.com>
2024-05-23 23:47:15 -0400

    conditional navigation on main navigation

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -6,14 +6,20 @@ import { HomeScreen } from '../screens/HomeScreen';
 import { ProductScreen } from '../screens/ProductScreen';
 import { HomeStackNavigator } from '../navigation/HomeStackNavigator';
 import { BottomTabNavigator } from '../navigation/BottomTabNavigator';
+import SignUpScreen from '../screens/SignUpScreen';
+import LoginScreen from '../screens/LoginScreen';
+import { AuthStackNavigator } from './AuthStackNavigator';
+import { useSelector } from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 export const MainNavigation = () => {
+  const { user } = useSelector((state) => state.auth.value);
+  console.log({ MainNavigationUSER: user });
+
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
-      {/*  <HomeStackNavigator /> */}
+      {user ? <BottomTabNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 };
